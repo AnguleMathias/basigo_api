db522ad52dc8445e8f601b7fe79a1aaa4870593b
Mathias Angule <angulemathias3@gmail.com>
2022-06-11 15:53:35 +0300

    netlify deploy updates

---
+++ src/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+// create express app
+const app = express();
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+
+const corsOptions = {
+  origin: "*",
+
+  methods: ["GET", "POST", "PUT", "DELETE"],
+
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+// netlify
+const expressRouter = express.Router();
+app.use("/.netlify/functions/server", expressRouter);
+
+// use cross origin resource sharing
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+  res.send("Hello from Basigo service!");
+});
+
+// routes
+const router = require("./routes/routes");
+app.use("/", router);
+
+// listen for requests
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
